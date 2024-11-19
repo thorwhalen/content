74b56fa23d430b66692cb40061a1aeeaabbdb149
Thor Whalen <1906276+thorwhalen@users.noreply.github.com>
2024-11-19 15:56:13 +0100

    Update content_access_code.py

--- py/content_access_code.py
+++ py/content_access_code.py
@@ -1,7 +1,14 @@
-"""Code to access thorwhalen/content data with ease"""
+"""Code to access thorwhalen/content data with ease
 
-branch = 'master'
-content_url = ('https://raw.githubusercontent.com/thorwhalen/content/' + branch + '/{}').format
+Note on requirements:
+Minimum:   pip install graze
+Optionally (for get_table function): pip install pandas
+
+"""
+
+org, repo, branch = 'thorwhalen/content/master'.split('/')
+content_url = (f'https://raw.githubusercontent.com/{org}/{repo}/{branch}' + '/{}').format  # function returning url of raw content from 
+
 
 def get_content_bytes(key, max_age=None):
     """Get bytes of content from `thorwhalen/content`, automatically caching locally.
@@ -13,6 +20,7 @@ def get_content_bytes(key, max_age=None):
     """
     from graze import graze
     return graze(content_url(key), max_age=max_age)
+
 
 def get_table(key, max_age=None, **extra_pandas_kwargs):
     """Get pandas dataframe from `thorwhalen/content`, automatically caching locally.
